77884f42c97e646b5353e5a7bdafd49e9d08fbdd
htntang <htntang@gmail.com>
2023-06-20 15:19:37 -0600

    initialized project

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="App.css" /> 
+
+      hello cuties
+    </>
+  )
+}
+
+export default App
